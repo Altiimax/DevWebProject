4e1293be3bee59c6d5f1d1461a00e3664b45771c
MMichotte <martin.michotte@gmail.com>
2020-03-19 09:34:07 +0100

    modified db scheme

--- toolbox_website/DB/data/data.sql
+++ toolbox_website/DB/data/data.sql
@@ -4,24 +4,25 @@
 
 --inserts
 
-insert into Countries (Id_CountryCode, CountryName)
+insert into "Countries" ("id_countryCode", countryname)
 values  ('BE', 'Belgique'),
         ('FR', 'France');
 
-insert into Towns (PostCode, TownName,Id_CountryCode)
+insert into "Towns" ("postCode", "townName","id_countryCode")
 values  (1330, 'Rixensart','BE'),
         (1000, 'Bruxelles','BE');
 
-insert into Persons (LastName, FirstName,Alias,BirthDate,Email)
+insert into "Persons" ("lastName", "firstName",alias,"birthDate",email)
 values  ('Gordon','Harry E ','HEGy','1969-10-07','marietta.kel@hotmail.com'),
         ('Sheridan','Dorothy',Null,'1973-01-01','garth2002@hotmail.com'),
         ('Doak','Kristine','shDoak','1983-03-18','favian1972@yahoo.com');
 
-insert into PersonsTowns (Id_Person,Id_Town)
+insert into "PersonsTowns" (id_person,id_town)
 values (1,2);
 
-insert into Tools (Id_Person,ToolName,ToolDescription,ToolPrice)
+insert into "Tools" (id_person,"toolName","toolDescription","toolPrice")
 values (1,'Tronçonneuse','Tronçonneuse de 50cm en excellent état',10.3);
+
 
 
  
