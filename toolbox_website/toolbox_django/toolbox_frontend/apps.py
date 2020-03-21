49273e558eef94fdca0035eb156d0760b6c3803c
Altiimax <44198671+Altiimax@users.noreply.github.com>
2020-03-21 10:45:18 +0100

    squelette react

--- toolbox_website/toolbox_django/toolbox_frontend/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ToolboxFrontendConfig(AppConfig):
-    name = 'toolbox_frontend'
--- toolbox_website/toolbox_django/toolbox_frontend/src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import tools from './tools';
-
-export default combineReducers({
-    tools
-});
