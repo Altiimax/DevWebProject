4a46c29799ef0afabee1b5f935a23e975637af80
MMichotte <martin.michotte@gmail.com>
2020-03-21 18:04:27 +0100

    added get user by email

--- toolbox_website/toolbox_django/toolbox_app/api.py
+++ toolbox_website/toolbox_django/toolbox_app/api.py
@@ -9,21 +9,21 @@ from .serializers import *
 
 
 class personViewSet(viewsets.GenericViewSet):
-    #queryset = Persons.objects.all().order_by('lastName')
-    #serializer_class = personsSerializer
 
-
-    #@detail_route(methods=['get'])
-    #detail
-    @action(detail=True, url_path='alias')
-    def getAlias(self, request, *args, **kwargs):
-        return Response({'alias': self.alias})
-
+    
     #127.0.0.1:8000/api/persons/all 
     @action(detail=False)
     def all(self, request, *args, **kwargs):
-        """" List all users """
+        """" list all users """
         queryset = Persons.objects.all().order_by('lastName')
+        serializer = personsSerializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    #127.0.0.1:8000/api/persons/aliases
+    @action(detail=False)
+    def aliases(self, request, *args, **kwargs):
+        """" get all used aliases"""
+        queryset = Persons.objects.all()
         serializer = personsSerializer(queryset, many=True)
         return Response(serializer.data)
     
@@ -32,13 +32,16 @@ class personViewSet(viewsets.GenericViewSet):
     def byemail(self, request, *args, **kwargs):
         """" get user by its email"""
         email = request.query_params.get('email')
-        queryset = Persons.objects.all().filter(email=email)
-        serializer = personsSerializer(queryset)
-        return Response(serializer.data)
-        try:
-            True
-        except:
-            error = "no user with this email: %s",email
+        queryset = Persons.objects.filter(email=email)
+        print(queryset)
+        if not queryset:
+            error = "no user with this email: %s"%(email)
             return Response({'error': error})
+        else:
+            serializer = personsSerializer(queryset,many=True)
+            return Response(serializer.data)
 
-        
+    #127.0.0.1:8000/api/persons/1/tools
+    @action(detail=True, methods=['get'])
+    def tools(self, request, *args, **kwargs):
+        return Response({'alias': self.alias})
