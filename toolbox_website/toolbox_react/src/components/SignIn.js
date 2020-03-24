b9ec820bdb854a8358043934a5a69891ee37273e
AllanFontaine <60710076+AllanFontaine@users.noreply.github.com>
2020-03-24 03:55:49 +0100

    Some test in css

--- toolbox_website/toolbox_react/src/components/SignIn.js
+++ toolbox_website/toolbox_react/src/components/SignIn.js
@@ -4,27 +4,27 @@ import React, { Component } from 'react'
  * This component is the formular the user must fill to connect 
  * to his account.
  * @param 
- * @return "XML form"  
+ * @return 'XML form'  
  */
 class SignIn extends Component {
     render() {
         return (
-            <form className="FormSign">
+            <form className='FormSign'>
                 <div>
-                    <label className="FormField_Label" htmlFor='alias'>Alias </label> 
-                    <input type='text' name ='alias' placeholder='Enter your alias'/>
+                    <label className='FormField_Label' htmlFor='alias'>Alias </label> 
+                    <input type='text' className='FormField_Input' name ='alias' placeholder='Enter your alias'/>
                 </div>
                 <div>
-                    <label className="FormField_Label" htmlFor='email'>E-mail address </label> 
-                    <input type='email' name ='email' placeholder='Enter your e-mail address'/>
+                    <label className='FormField_Label' htmlFor='email'>E-mail address </label> 
+                    <input type='email' className='FormField_Input' name ='email' placeholder='Enter your e-mail address'/>
                 </div>
                 <div>
-                    <label className="FormField_Label" htmlFor='password'>Password </label> 
-                    <input type='password' name ='password' placeholder='Enter your password'/>
+                    <label className='FormField_Label' htmlFor='password'>Password </label> 
+                    <input type='password' className='FormField_Input' name ='password' placeholder='Enter your password'/>
             </div>
             <div>
-                <button className="nokButton">Cancel</button>
-                <button className="okButton">Confirm</button>
+                <button className='nokButton'>Cancel</button>
+                <button className='okButton'>Confirm</button>
             </div>  
             </form>
         )
--- toolbox_website/toolbox_react/src/components/SignUp.js
+++ toolbox_website/toolbox_react/src/components/SignUp.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react'
 
 /**
  * This component is used to register the information set by 
@@ -6,43 +6,49 @@ import React from 'react';
  * email address, alias, ... 
  * @param
  */
-const SignUp = () => {
-    return(
-        <form className="FormSign">
-            <div>
-                <label className="FormField_Label" htmlFor='email'>E-mail address </label>
-                <input type='email' className='FormField_Input' name ='email' placeholder='Enter your e-mail address'/>
-            </div>
-            <div>
-                <label className="FormField_Label" htmlFor='forename'>First name </label>
-                <input type='text' className='FormField_Input' name ='forename' placeholder='Enter your forename'/>
-            </div>
-            <div>
-                <label className="FormField_Label" htmlFor='surename'>Last name </label>
-                <input type='text' className='FormField_Input' name ='surename' placeholder='Enter your surename'/>
-            </div>
-            <div>
-                <label className="FormField_Label" htmlFor='birthDate'>Birthdate </label>
-                <input type='date' className='FormField_Input' name ='birthDate'/>
-            </div>
-            <div>
-                <label className="FormField_Label" htmlFor='alias'>Alias </label>
-                <input type='text' className='FormField_Input' name ='alias' placeholder='Enter your alias'/>
-            </div>
-            <div>
-                <label className="FormField_Label" htmlFor='newPassword'>New password </label>
-                <input type='password' className='FormField_Input' name ='newPassword' placeholder='Enter a new password'/>
-            </div>
-            <div>
-                <label className="FormField_Label" htmlFor='confirmPassword'>Password confirmation </label>
-                <input type='password' className='FormField_Input' name ='confirmPassword' placeholder='Verify your password'/>
-            </div>
-            <div>
-                <button className="nokButton">Cancel registration</button>
-                <button className="okButton">Complete registration</button>
-            </div>
-        </form>
-    );
+class SignUp extends Component {
+    render() {
+        return (
+            <form className='FormSign'>
+                <div>
+                    <label className='FormField_Label' htmlFor='email'>E-mail address </label>
+                    <input type='email' className='FormField_Input' name ='email' placeholder='Enter your e-mail address'/>
+                </div>
+                <div>
+                    <label className='FormField_Label' htmlFor='forename'>First name </label>
+                    <input type='text' className='FormField_Input' name ='forename' placeholder='Enter your forename'/>
+                </div>
+                <div>
+                    <label className='FormField_Label' htmlFor='surename'>Last name </label>
+                    <input type='text' className='FormField_Input' name ='surename' placeholder='Enter your surename'/>
+                </div>
+                <div>
+                    <label className='FormField_Label' htmlFor='birthDate'>Birthdate </label>
+                    <input type='date' className='FormField_Input' name ='birthDate'/>
+                </div>
+                <div>
+                    <label className='FormField_Label' htmlFor='alias'>Alias </label>
+                    <input type='text' className='FormField_Input' name ='alias' placeholder='Enter your alias'/>
+                </div>
+                <div>
+                    <label className='FormField_Label' htmlFor='newPassword'>New password </label>
+                    <input type='password' className='FormField_Input' name ='newPassword' placeholder='Enter a new password'/>
+                </div>
+                <div>
+                    <label className='FormField_Label' htmlFor='confirmPassword'>Password confirmation </label>
+                    <input type='password' className='FormField_Input' name ='confirmPassword' placeholder='Verify your password'/>
+                </div>
+                <div>
+                    <label className='FormField_CheckBox' htmlFor='agreements'/>
+                    <input type='checkbox' className='FormField_Input_Check' name='agreements'/> I agree with all the statements in <a href='' className='FormField_TermsLink'>terms of service.</a>
+                </div>
+                <div>
+                    <button className='nokButton'>Cancel registration</button>
+                    <button className='okButton'>Complete registration</button>
+                </div>
+            </form>
+        );
+    }
 }
 
 export default SignUp;
