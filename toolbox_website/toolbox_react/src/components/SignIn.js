4767e142a20f77d4bfa73d2ee848b0b5314e0485
AllanFontaine <60710076+AllanFontaine@users.noreply.github.com>
2020-03-25 02:48:22 +0100

    Installation for the communication with the api
    Preparation of the form to be pushed to db

--- toolbox_website/toolbox_react/src/components/SignIn.js
+++ toolbox_website/toolbox_react/src/components/SignIn.js
@@ -8,29 +8,60 @@ import {Link} from 'react-router-dom';
  * @return 'XML form'  
  */
 class SignIn extends Component {
+    
+    constructor(){
+        super();
+
+        this.state ={
+            alias:'',
+            email:'',
+            password:''
+        }
+        
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        let target = e.target;
+        let value = target.type === 'checkbox' ? target.checked : target.value;
+        let name = target.name;
+
+        this.setState({
+          [name]: value
+        });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        console.log('The form was submitted with the following data:');
+        console.log(this.state);
+    }
+
     render() {
         return (
             <div className='FormSign'>
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <div>
                     <label className='FormField_Label' htmlFor='alias'>Alias </label> 
-                    <input type='text' className='FormField_Input' name ='alias' placeholder='Enter your alias'/>
+                    <input type='text' className='FormField_Input' name ='alias' placeholder='Enter your alias' value={this.state.alias} onChange={this.handleChange}/>
                 </div>
                 <div>
                     <label className='FormField_Label' htmlFor='email'>E-mail address </label> 
-                    <input type='email' className='FormField_Input' name ='email' placeholder='Enter your e-mail address'/>
+                    <input type='email' className='FormField_Input' name ='email' placeholder='Enter your e-mail address' value={this.state.email} onChange={this.handleChange}/>
                 </div>
                 <div>
                     <label className='FormField_Label' htmlFor='password'>Password </label> 
-                    <input type='password' className='FormField_Input' name ='password' placeholder='Enter your password'/>
+                    <input type='password' className='FormField_Input' name ='password' placeholder='Enter your password' value={this.state.password} onChange={this.handleChange}/>
                 </div>
                 <div>
-                    <button className='nokButton'>Cancel</button>
-                    <div class="divider"/>
-                    <button className='okButton'>Confirm</button>
+                    <button type='reset' className='nokButton'>Cancel</button>
+                    <div className="divider"/>
+                    <button type='submit' className='okButton'>Confirm</button>
                 </div>  
             </form>
-            <div class="divider"/>
+            <div className="divider"/>
                 <div>
                     <button className='goBackMenuButton'><Link to='/' className='FormField_Link'>Go back to the Menu</Link></button>
                 </div>
--- toolbox_website/toolbox_react/src/components/SignUp.js
+++ toolbox_website/toolbox_react/src/components/SignUp.js
@@ -8,51 +8,87 @@ import {Link} from 'react-router-dom';
  * @param
  */
 class SignUp extends Component {
+
+    constructor(){
+        super();
+
+        this.state ={
+            email:'',
+            firstname:'',
+            lastname:'',
+            birthdate:'',
+            alias:'',
+            newPassword:'',
+            confirmPassword:'',
+            hasagreed: false
+        }
+        
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        let target = e.target;
+        let value = target.type === 'checkbox' ? target.checked : target.value;
+        let name = target.name;
+
+        this.setState({
+          [name]: value
+        });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        console.log('The form was submitted with the following data:');
+        console.log(this.state);
+    }
+
     render() {
         return (
             <div className='FormSign'>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <div>
                         <label className='FormField_Label' htmlFor='email'>E-mail address </label>
-                        <input type='email' className='FormField_Input' name ='email' placeholder='Enter your e-mail address'/>
+                        <input type='email' className='FormField_Input' name ='email' placeholder='Enter your e-mail address' value={this.state.email} onChange={this.handleChange}/>
                     </div>
                     <div>
-                        <label className='FormField_Label' htmlFor='forename'>First name </label>
-                        <input type='text' className='FormField_Input' name ='forename' placeholder='Enter your forename'/>
+                        <label className='FormField_Label' htmlFor='firstname'>First name </label>
+                        <input type='text' className='FormField_Input' name ='firstname' placeholder='Enter your forename' value={this.state.firstname} onChange={this.handleChange}/>
                     </div>
                     <div>
-                        <label className='FormField_Label' htmlFor='surename'>Last name </label>
-                        <input type='text' className='FormField_Input' name ='surename' placeholder='Enter your surename'/>
+                        <label className='FormField_Label' htmlFor='lastname'>Last name </label>
+                        <input type='text' className='FormField_Input' name ='lastname' placeholder='Enter your surename' value={this.state.lastname} onChange={this.handleChange}/>
                     </div>
                     <div>
                         <label className='FormField_Label' htmlFor='birthDate'>Birthdate </label>
-                        <input type='date' className='FormField_Input' name ='birthDate'/>
+                        <input type='date' className='FormField_Input' name ='birthDate' value={this.state.birthdate} onChange={this.handleChange}/>
                     </div>
                     <div>
                         <label className='FormField_Label' htmlFor='alias'>Alias </label>
-                        <input type='text' className='FormField_Input' name ='alias' placeholder='Enter your alias'/>
+                        <input type='text' className='FormField_Input' name ='alias' placeholder='Enter your alias' value={this.state.alias} onChange={this.handleChange}/>
                     </div>
                     <div>
                         <label className='FormField_Label' htmlFor='newPassword'>New password </label>
-                        <input type='password' className='FormField_Input' name ='newPassword' placeholder='Enter a new password'/>
+                        <input type='password' className='FormField_Input' name ='newPassword' placeholder='Enter a new password' value={this.state.newPassword} onChange={this.handleChange}/>
                     </div>
                     <div>
                         <label className='FormField_Label' htmlFor='confirmPassword'>Password confirmation </label>
-                        <input type='password' className='FormField_Input' name ='confirmPassword' placeholder='Verify your password'/>
+                        <input type='password' className='FormField_Input' name ='confirmPassword' placeholder='Verify your password' value={this.state.confirmPassword} onChange={this.handleChange}/>
                     </div>
-                    <div class="divider"/>
+                    <div className='divider'/>
                     <div>
-                        <label className='FormField_CheckBox' htmlFor='agreements'/>
-                        <input type='checkbox' className='FormField_Input_Check' name='agreements'/> I agree with all the statements in <a href='' className='FormField_TermsLink'>terms of service.</a>
+                        <label className='FormField_CheckBox' htmlFor='hasagreed'/>
+                        <input type='checkbox' className='FormField_Input_Check' name='hasagreed' value={this.state.hasagreed} onChange={this.handleChange}/> I agree with all the statements in <a href='' className='FormField_TermsLink'>terms of service.</a>
                     </div>
-                    <div class="divider"/>
+                    <div className='divider'/>
                     <div>
-                        <button className='nokButton'>Cancel registration</button>
-                        <div class="divider"/>
-                        <button className='okButton'>Complete registration</button>
+                        <button type='reset' className='nokButton'>Cancel registration</button>
+                        <div className='divider'/>
+                        <button type='submit' className='okButton'>Complete registration</button>
                     </div>
                 </form>
-                <div class="divider"/>
+                <div className='divider'/>
             <div>
                 <button className='goBackMenuButton'><Link to='/' className='FormField_Link'>Go back to the Menu</Link></button>
             </div>
