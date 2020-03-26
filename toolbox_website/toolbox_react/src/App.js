458b5597d2d54111f52eb2ed51aa1780a5a6951e
MMichotte <martin.michotte@gmail.com>
2020-03-26 19:12:24 +0100

    added api request from frontend example

--- toolbox_website/toolbox_react/src/App.js
+++ toolbox_website/toolbox_react/src/App.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {apiRequest} from './apiRequest.js';
+
+  let req = new apiRequest();
+  req.open("GET", "http://127.0.0.1:8000/api/persons/");
+  req.addEventListener("readystatechange", function() {
+    if(this.readyState === 4) {
+      console.log("resp status :" + this.status);
+      console.log("resp text :" + this.responseText);
+    }
+  });
+  
+  req.send();
+
 
 function App() {
   return (
