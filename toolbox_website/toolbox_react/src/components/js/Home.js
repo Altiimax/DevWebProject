a03fc75e9f64195e14038a807051f56ef921b94a
AllanFontaine <60710076+AllanFontaine@users.noreply.github.com>
2020-04-21 21:45:18 +0200

    Form first approach and small css

--- toolbox_website/toolbox_react/src/components/js/Home.js
+++ toolbox_website/toolbox_react/src/components/js/Home.js
@@ -1,13 +1,40 @@
 import React from "react";
 import "../css/Home.css";
-import logo from "../../nut.svg";
+import { Form, Col } from "react-bootstrap";
+//import logo from "../../nut.svg";
 
 export default function Home() {
   return (
-    <div className="App">
-      <section className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </section>
+    <div className="home">
+      <div className="container">
+        <Form className="baseForm">
+          <Form.Group>
+            <Form.Label>Which tool?</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Wrench, saw, screwdriver, ..."
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Where?</Form.Label>
+            <Form.Control type="text" placeholder="Select a city by name" />
+          </Form.Group>
+          <Form.Row>
+            <Form.Group as={Col}>
+              <Form.Label>From</Form.Label>
+              <Form.Control type="date" />
+            </Form.Group>
+            <Form.Group as={Col}>
+              <Form.Label>To</Form.Label>
+              <Form.Control type="date" />
+            </Form.Group>
+          </Form.Row>
+          <button className="searchButton" type="submit">
+            Research
+          </button>
+        </Form>
+      </div>
     </div>
   );
 }
+//<img src={logo} className="App-logo" alt="logo" />
