e7fe05aeace818b1a7b53660e1fe3eadadbd2135
Altiimax <44198671+Altiimax@users.noreply.github.com>
2020-04-21 23:32:30 +0200

    Conflict management

--- toolbox_website/toolbox_react/src/App.js
+++ toolbox_website/toolbox_react/src/App.js
@@ -6,15 +6,17 @@ import Footer from "./components/js/Footer";
 
 function App() {
   return (
-    <>
+    <div className="page-container">
       <header>
         <Header />
       </header>
-      <Route />
+      <div className="content-wrap">
+        <Route />
+      </div>
       <footer>
         <Footer />
       </footer>
-    </>
+    </div>
   );
 }
 
