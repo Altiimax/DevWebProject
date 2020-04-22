d4bef5b563a1c1779104da03f79148379930eba1
Altiimax <44198671+Altiimax@users.noreply.github.com>
2020-04-22 03:09:31 +0200

    Profile page idea

--- toolbox_website/toolbox_react/src/components/js/Profile.js
+++ toolbox_website/toolbox_react/src/components/js/Profile.js
@@ -1,6 +1,7 @@
-import React, { Component } from "react";
+import React, { Component, Table } from "react";
 import { Link } from "react-router-dom";
 import Greet from "./Greetings";
+import "../css/Profile.css";
 /**
  * This component is used to display the account informations
  * of the member.
@@ -17,17 +18,57 @@ class Profile extends Component {
     document.getElementById("myGroupsNav").style.display = "block";
     document.getElementById("myToolsNav").style.display = "block";
     document.getElementById("myProfileNav").style.display = "block";
-    document.getElementById("myToolsNav").style.display = "block";
     document.getElementById("signUpNav").style.display = "none";
     document.getElementById("signInNav").style.display = "none";
     document.getElementById("signOutNav").style.display = "block";
   };
 
+  //render les tables en claire avec de fausses infos, il faudra y mettre les infos de profil
+  //et mettre le tout dans un side bar pour display que ce qu'il faut.
   render() {
     return (
-      <>
-        <Greet className="error" name="Host" />
-      </>
+      <div className="myProfile">
+        <div className="container">
+          <Greet className="error" name="Host" />
+          <div className="container">
+            <table id="myTools">
+              <thead>
+                <tr>
+                  <th>Image</th>
+                  <th>Name</th>
+                  <th>Owner</th>
+                  <th>Renting infos</th>
+                  <th>State</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Image not found</td>
+                  <td>Screwdriver</td>
+                  <td>Bob</td>
+                  <td>
+                    Was borrowed on 20/04/2012 and must be given back before
+                    24/05/2012
+                  </td>
+                  <td>Used</td>
+                </tr>
+                <tr>
+                  <td>Image not found</td>
+                  <td>Screwdriver</td>
+                  <td>Bob</td>
+                  <td>
+                    Was borrowed on 20/04/2012 and must be given back before
+                    24/05/2012
+                  </td>
+                  <td>Used</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+
+          <table id="myGroups"></table>
+        </div>
+      </div>
     );
   }
 }
