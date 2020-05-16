527ff63c185f63edffb946798b126f644a7fbf0a
Altiimax <44198671+Altiimax@users.noreply.github.com>
2020-05-16 15:54:52 +0200

    find tools advancement

--- toolbox_website/toolbox_react/src/components/FindTools/FindTools.js
+++ toolbox_website/toolbox_react/src/components/FindTools/FindTools.js
@@ -1,8 +1,70 @@
 import React, { Component } from "react";
 import { Form /*Col*/ } from "react-bootstrap";
 import "./FindTools.css";
+import { apiRequest } from "../../api/apiRequest.js";
+import Search from "../Search/Search.js";
+import ReactDOM from "react-dom";
 
 class FindTools extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toolName :"",
+      townName:"",
+      data:[ {
+        "id_groupName": "TestGroup1",
+        "groupType": "public",
+        "groupDescription": null,
+        "groupRange": 50,
+        "town": {
+          "id_town": 1,
+          "postCode": 1330,
+          "townName": "Rixensart",
+          "id_countryCode": "BE"
+        }
+      }]
+      }
+  }
+
+apiFindTools = () => {
+let self= this;
+let endpoint = "/api/search/?what='"+self.state.toolName+"'&where='"+self.state.townName+"'";
+
+    let req = new apiRequest();
+    req.open("GET", `${endpoint}`);
+
+    req.addEventListener("readystatechange", function () {
+      if (this.readyState === 4) {
+        if (this.status === 200) {
+          let rep = JSON.parse(this.responseText);
+          if(self.state.data.length === 0){
+            alert("Tool not found");
+          }
+          else{
+            console.log(self.state.data)
+            ReactDOM.render(<Search data={self.state.data} />, document.getElementById("list"));
+          }
+        }
+      }
+    });
+
+    req.send();
+console.log(this.state.toolName);
+console.log(this.state.townName);
+}
+
+handleChange = (e) => {
+  let target = e.target;
+  let value = target.value;
+  let name = target.name;
+  this.setState({ [name]: value });
+};
+
+handleSubmit = (e) =>{
+e.preventDefault();
+this.apiFindTools();
+}
+
   render() {
     return (
       <div className="searchForm">
@@ -12,12 +74,14 @@ class FindTools extends Component {
             <Form.Label>Which tool?</Form.Label>
             <Form.Control
               type="text"
+              name="toolName"
               placeholder="Wrench, saw, screwdriver, ..."
+              onChange={this.handleChange}
             />
           </Form.Group>
           <Form.Group>
             <Form.Label>Where?</Form.Label>
-            <Form.Control type="text" placeholder="Select a city by name" />
+            <Form.Control type="text" placeholder="Select a city by name" name="townName" onChange={this.handleChange}/>
           </Form.Group>
           {/* //TODO pour le moment on implémente pas cette fonctionalitée ;) 
           <Form.Row>
@@ -31,7 +95,7 @@ class FindTools extends Component {
             </Form.Group>
           </Form.Row>*/}
           <div className="searchButtonContainer">
-            <button className="searchButton" type="submit">
+            <button className="searchButton" type="submit"  onClick={this.handleSubmit}>
               Search
             </button>
           </div>
--- toolbox_website/toolbox_react/src/pages/home/Home.js
+++ toolbox_website/toolbox_react/src/pages/home/Home.js
@@ -1,15 +1,32 @@
 import React from "react";
 import FindTools from "../../components/FindTools/FindTools.js";
 import "./Home.css";
+import Search from "../../components/Search/Search.js";
+import Map from "../../components/Map/Map.js";
 //import Test from "../../components/test.js";
 
 export default function Home() {
+  const data2 =  [ {
+    "id_groupName": "TestGroup1",
+    "groupType": "public",
+    "groupDescription": null,
+    "groupRange": 50,
+    "town": {
+      "id_town": 1,
+      "postCode": 1330,
+      "townName": "Rixensart",
+      "id_countryCode": "BE"
+    }
+  }]
   return (
     <div className="Home">
       <div className="SearchFrom">
         <FindTools />
       </div>
       <div className="QuickLook">Ceci est provisoire</div>
+      <Search data={data2}/>
+      <div className="searchList" id="list"></div>
+      <div className="searchMap"></div>
     </div>
   );
 }
